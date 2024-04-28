966151f813aff1d51b64f3e28a2d02d67eb1fd53
Mateusz Grzonka <118964850+mateushhh@users.noreply.github.com>
2024-04-28 23:17:43 +0200

    Fixes

--- hex.cpp
+++ hex.cpp
@@ -1,116 +1,145 @@
 ﻿#include <iostream>
 #include <string>
+#include <algorithm>
 
 #define MAX_SIZE 11
 
-int columnCounter(std::string line) {
-	int columnCounter = 0;
-	for (int i = 0; i < line.length(); i++)
-		if (line[i] == '>')
-			columnCounter += 1;
-	return columnCounter;
-}
+class Board {
+public:
+	char board[MAX_SIZE][MAX_SIZE];
+	int size;
+	int bpawns;
+	int rpawns;
 
-void readBoard(char(*board)[MAX_SIZE], int& boardsize, int& pawnsnumber) {
-	std::string line;
-	int cursori, cursorj;
-	int width = 0, tempj = 1;
-	bool upperpart = true;
-	bool read = true;
-	pawnsnumber = 0;
-
-	for (int i = 0; i < MAX_SIZE; i++) {
-		for (int j = 0; j < MAX_SIZE; j++)
-			board[i][j] = ' ';
+	int columnCounter(std::string line) {
+		int columnCounter = 0;
+		for (int i = 0; i < line.length(); i++)
+			if (line[i] == '>')
+				columnCounter += 1;
+		return columnCounter;
 	}
 
-	std::getline(std::cin, line);
-	while (read) {
-		std::getline(std::cin, line);
-		if (width < columnCounter(line)) {
-			width = columnCounter(line);
-		}
-		else {
-			upperpart = false;
+	void read() {
+		std::string line;
+		int cursori, cursorj;
+		int width = 0, tempj = 1;
+		bool upperpart = true;
+		bool read = true;
+		rpawns = 0;
+		bpawns = 0;
+
+		for (int i = 0; i < MAX_SIZE; i++) {
+			for (int j = 0; j < MAX_SIZE; j++)
+				board[i][j] = ' ';
 		}
 
-		if (upperpart) {
-			cursori = width - 1;
-			cursorj = 0;
-		}
-		else {
-			cursori = width - 1;
-			cursorj = tempj++;
-		}
+		while (read) {
+			std::getline(std::cin, line);
+			if (width < columnCounter(line)) {
+				width = columnCounter(line);
+			}
+			else {
+				upperpart = false;
+			}
 
-		for (int i = 0; i < line.length(); i++) {
-			if (line[i] == '-' && line[i - 1] == '-' && line[i - 2] == '-') {
-				read = false;
-				break;
+			if (upperpart) {
+				cursori = width - 1;
+				cursorj = 0;
 			}
-			else if (line[i] == 'r' || line[i] == 'b') {
-				board[cursori][cursorj] = line[i];
-				pawnsnumber += 1;
+			else {
+				cursori = width - 1;
+				cursorj = tempj++;
 			}
-			else if (line[i] == '>') {
-				cursori--;
-				cursorj++;
+
+			for (int i = 0; i < line.length(); i++) {
+				if (line[i] == '-' && line[i - 1] == '-' && line[i - 2] == '-') {
+					read = false;
+					break;
+				}
+				else if (line[i] == 'r') {
+					board[cursori][cursorj] = 'r';
+					rpawns += 1;
+				}
+				else if (line[i] == 'b') {
+					board[cursori][cursorj] = 'b';
+					bpawns += 1;
+				}
+				else if (line[i] == '>') {
+					cursori--;
+					cursorj++;
+				}
 			}
+		}
+		size = width;
+	}
+
+	void draw() {
+		for (int i = 0; i < MAX_SIZE; i++) {
+			for (int j = 0; j < MAX_SIZE; j++)
+				std::cout << board[i][j] << ',';
+			std::cout << '\n';
 		}
 	}
-	boardsize = width;
-}
 
-void drawBoard(char(*board)[MAX_SIZE]) {
-	for (int i = 0; i < MAX_SIZE; i++) {
-		for (int j = 0; j < MAX_SIZE; j++)
-			std::cout << board[i][j] << ',';
-		std::cout << '\n';
+	int pawnsNumber() {
+		return (bpawns + rpawns);
 	}
-}
+
+	int boardSize() {
+		return size;
+	}
+
+	bool isBoardCorrect() {
+		return ((rpawns - 1 == bpawns) || (rpawns == bpawns));
+	}
+};
+
 
 int main() {
-	char board[MAX_SIZE][MAX_SIZE];
-	int boardsize, pawnsnumber;
+	Board board;
 	std::string command;
-
-	//readBoard(board, boardsize, pawnsnumber);
-	//drawBoard(board);
 
 	while (std::getline(std::cin, command)) {
 		command.erase(std::remove_if(command.begin(), command.end(), ::isspace), command.end());
 		if (command == "---")
-			readBoard(board, boardsize, pawnsnumber);
+			board.read();
+		else if (command == "DRAW")
+			board.draw();
 		else if (command == "BOARD_SIZE")
-			std::cout << boardsize << '\n';
+			std::cout << board.boardSize() << '\n';
 		else if (command == "PAWNS_NUMBER")
-			std::cout << pawnsnumber << '\n';
+			std::cout << board.pawnsNumber() << '\n';
+		else if (command == "IS_BOARD_CORRECT") {
+			if (board.isBoardCorrect())
+				std::cout << "YES\n";
+			else
+				std::cout << "NO\n";
+		}
 	}
 	return 0;
 }
 
-	/*
-	TODO List:
-	(8 %)	BOARD_SIZE - (test 1)
-	(8 %)	PAWNS_NUMBER - (test 2)
-	(8 %)	IS_BOARD_CORRECT - (test 3)
-	(20 %)	IS_GAME_OVER - (test 4)
-	(20 %)	IS_BOARD_POSSIBLE - (test 5)
-	(20 %)	CAN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT - (test 6)
-			CAN_BLUE_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT
-			CAN_RED_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT
-			CAN_BLUE_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT
-	(16 %)	CAN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT
-			CAN_BLUE_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT
-			CAN_RED_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT
-			CAN_BLUE_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT
+/*
+TODO List:
+(8 %)	BOARD_SIZE - (test 1)
+(8 %)	PAWNS_NUMBER - (test 2)
+(8 %)	IS_BOARD_CORRECT - (test 3)
+(20 %)	IS_GAME_OVER - (test 4)
+(20 %)	IS_BOARD_POSSIBLE - (test 5)
+(20 %)	CAN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT - (test 6)
+		CAN_BLUE_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT
+		CAN_RED_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT
+		CAN_BLUE_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT
+(16 %)	CAN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT
+		CAN_BLUE_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT
+		CAN_RED_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT
+		CAN_BLUE_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT
 
-		   ---
-		--< r >--
-	 --< b >-<   >--
-	<   >-< b >-<   >
-	 --<   >-< r >--
-		--< r >--
-		   ---
-	*/
-
+	   ---
+	--< r >--
+ --< b >-<   >--
+<   >-< b >-<   >
+ --<   >-< r >--
+	--< r >--
+	   ---
+*/
